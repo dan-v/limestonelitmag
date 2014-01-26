a0b85e59ae4d18d5d6f0ca9ce8eb3eeea4f8e28d
Dan <1596016+dan-v@users.noreply.github.com>
2014-01-25 17:48:15 -0800

    Add archives, virtualenv requirements.txt, and inital fixtures data

--- limestone/urls.py
+++ limestone/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
 	url(r'^submit', views.submit, name='submit'),
 	url(r'^about', views.about, name='about'),
-	url(r'^archives', views.archives, name='archives'),
-	url(r'^currentissue', views.currentissue, name='currentissue'),
+	url(r'^archive', views.archives, name='archives'),
+	url(r'^issues/(?P<number>([0-9]+))/$', views.issue, name='issue')
 )
--- limestone/views.py
+++ limestone/views.py
@@ -11,18 +11,19 @@ def index(request):
 	return render(request, 'index.html', context)
 
 def archives(request):
-	return render(request, 'archive.html')
+	archived_issues = Issue.objects.filter(latest_issue='False', active_issue='True')
+	context = {'archived_issues': archived_issues}
+	return render(request, 'archive.html', context)
+
+def issue(request, number):
+	issue = Issue.objects.filter(id=number)
+	story_list_fiction = Story.objects.filter(issue=issue, story_type='Fiction')
+	story_list_poetry = Story.objects.filter(issue=issue, story_type='Poetry')
+	context = {'issue': issue, 'story_list_fiction': story_list_fiction, 'story_list_poetry': story_list_poetry}
+	return render(request, 'issue.html', context)	
 
 def about(request):
 	return render(request, 'about.html')
-
-def currentissue(request):
-	latest_issue = Issue.objects.filter(latest_issue='True', active_issue='True')
-	latest_story_list_fiction = Story.objects.filter(issue=latest_issue, story_type='Fiction')
-	latest_story_list_poetry = Story.objects.filter(issue=latest_issue, story_type='Poetry')
-	context = {'latest_issue': latest_issue, 'latest_story_list_fiction': latest_story_list_fiction, 'latest_story_list_poetry': latest_story_list_poetry}
-
-	return render(request, 'currentissue.html', context)
 
 def submit(request):
 	return render (request, 'submit.html')
